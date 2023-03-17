8570b8c04136f117a4cba807231f0d9391b0206a
belalghonaim99 <belalpop1999@yahoo.com>
2023-03-17 16:30:49 -0700

    updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
@@ -28,5 +30,3 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
-
