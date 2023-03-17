d4285d9960779535ab3d40a34a99955c5921bc78
belalghonaim99 <belalpop1999@yahoo.com>
2023-03-17 11:19:52 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main -A Program that print a line using print function
- * Return: 0 (Success)
- */
+* main -A Program that print a line using print function
+* Return: 0 (Success)
+*/
 int main(void)
 {
       printf("with proper grammar, but the outcome is a piece of art,\n")
