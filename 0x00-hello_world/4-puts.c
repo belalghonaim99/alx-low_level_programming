00b9ea24fb02aad861424afc608567a937d67eff
belalghonaim99 <belalpop1999@yahoo.com>
2023-03-17 09:32:41 -0700

    0-updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h>
+/**
+ *  main - program that print function
+ *  Return: 0 {Susccess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
