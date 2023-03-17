13463e19b90764568d5a0d95519a00d5c016fe53
belalghonaim99 <belalpop1999@yahoo.com>
2023-03-17 12:03:36 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-* main - A Program that print a line using print function
-* Return: 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
