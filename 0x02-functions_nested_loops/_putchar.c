3000e0e717d0aa42b5d9352d7478faff0c2c66fa
belalghonaim99 <belalpop1999@yahoo.com>
2023-03-21 11:37:32 -0700

    updated

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putcher - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The Character to print
  *
  * Retrun: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putcher(char c)
+int _putchar(char c)
 {
     return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifdef main_h
+#ifndef main_h
 #define main_h
 
 int __putchar(char);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #end if
