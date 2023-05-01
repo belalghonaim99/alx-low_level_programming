de50cce8c71663f6a55ebfe12f23ed6115e3c582
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-01 06:25:37 -0700

    updated

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,19 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * print_listint
- * linked list of type listint_t and print
- * Return: number of node
- */
-size_t print_listint(const listin_t *h)
-
-{
-	size_t num = 0;
+* listint_len - returns the number of elements in a linked lists
+*@h: linked list of type listint_t and print
+*
+*Return: number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{ 
+	size_t num = 0;	
+	
 	while (h)
 	{
-		printf("%d\n", h->n);
-	
-	num++
+	num++;
 	h = h->next;
 	}
-
-	return (num)
+	return (num);
 }	
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,15 +13,14 @@
  */
 typedef struct listint_s
 {
-	int n:
+	int n;
 		struct listint_s *next;
 
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listnint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-listin_t *add_nodeint_end(listint_t **head,const int n);
+listin_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
