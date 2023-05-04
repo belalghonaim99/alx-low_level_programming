d0f65f9d68349108bd0d42e9623776555cf3f492
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-04 10:21:00 -0700

    updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H	
 
-unsigned int_binary_to_unit(const char *b);
+unsigned int _binary_to_unit(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
