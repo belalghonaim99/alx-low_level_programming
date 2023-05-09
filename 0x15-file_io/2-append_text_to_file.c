61e6b022d7746860e8f43bc4909885557badbc9c
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-09 07:42:28 -0700

    updatd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,42 +1,33 @@
 #include "main.h"
 
 /**
- *  _strlen - return string
- * @s: check length of string
- *
- * Return: int
- */
-int _strlen(char *s)
-{
-        int i = 0;
-
-        if (!s)
-                return (0);
-
-        while (*s++)
-                i++;
-        return (i);
-}
-
-/**
- * create_file - create a file
- * @filename: name of the  file to create
- * @text_content: write to the  file
+ * append_text_to_file - append to file
+ * @filename: name of the  file to attend
+ * @text_content: added
  *
  * Return: if -1 fails , if 1 success
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd;
-        ssize_t wr = 0, len = _strlen(text_content);
+	int fd, wr, len = 0;
 
-        if (!filename)
-                return (-1);
-        fd = open(filename, O_WRONLY | O_APPEND);
-        if (fd == -1)
-                return (-1);
-        if (len)
-                wr = write(fd, text_content, len);
-        close(fd);
-        return (wr == len ? 1 : -1);
+
+	if (filename == 0)
+
+		return (-1);
+	if (text_content != 0)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	wr = write(fd, text_content, len);
+
+
+	if (fd == -1 || wr == -1)
+	return (1);
+	close(fd);
+	return (1);
 }
