5f721c01ca137780d2c9e6640c56a75cb5432157
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-09 06:23:46 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,7 @@ int _strlen(char *s)
 		i++;
 	return (i);
 }
+
 /**
  * create_file - create a file
  * @filename: name of the  file to create
@@ -31,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, 0_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, 0_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_TWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
