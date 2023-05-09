09f492615e27d8fe1e6708373685b7e85c920813
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-09 07:43:43 -0700

    updatd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,10 +12,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, wr, len = 0;
 
 
-	if (filename == 0)
+	if (filename == NULL)
 
 		return (-1);
-	if (text_content != 0)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
