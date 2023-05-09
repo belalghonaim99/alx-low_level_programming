e9d65ef0b487fe097460463349719ddc3f84b90c
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-09 03:57:43 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates file
  * @filename: name file to create
- *
+ *@text_content: text
  * Return: 1 if success & 0 if fail
  */
 int create_file(const char *filename, char *text_content)
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_TRUSR | S_TWUSR);
 	if (fd == -1)
 			return (-1);
 
