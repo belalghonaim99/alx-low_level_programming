7d4f83263111fcca730f9792110a65d34522511f
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-09 03:22:29 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	char buf[READ_BUF_SIZE * 8];
 
-	if (!filename || letters == 0)
+	if (!filename || !letters == 0)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -23,5 +23,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	w = read(fd, &buf[0], letters);
 	w = write(STDOUT_FILENO, &buf[0], w);
 	close(fd);
-	return (letters);
+	return (w);
 }
