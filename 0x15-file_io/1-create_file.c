5502c39297fb0c0f8ff1bef8e5be24f5949f3c97
belalghonaim99 <belalghonaim99@gmail.com>
2023-05-09 03:45:39 -0700

    updated

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: name file to create
+ *
+ * Return: 1 if success & 0 if fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t w = 0; len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+			return (-1);
+
+			if (len)
+
+			w = write(fd, text_content, len);
+			close(fd);
+			return (w == len ? 1 : -1);
+}
+
+/**
+ * _strlen - return string
+ * @s: chech the string
+ *
+ * Return: int of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
