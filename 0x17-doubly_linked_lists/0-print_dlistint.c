52b8d200d3fd85bd0628bb86c1b2ed89aa904201
belalghonaim99 <belalghonaim99@gmail.com>
2023-06-16 08:34:56 -0700

    updtaed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,21 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints elemnets 
+ * @h:  points to the previous node
+ * Return: numbers of nodes.
+ **/
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	const dlistint_t *node = h;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-	    printf("%d\n", h->n);
-	    h = h->next;
+	while (node)
+	{
+		printf("%i\n", node->n);
+		count++;
+		node = node->next;
+	}
 
-	    count++;
-    }
-
-    return (count);
+	return (count);
 }
