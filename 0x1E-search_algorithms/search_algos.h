1797f3a137b35994bc701e308c9ed3ebe5a7bc27
belalghonaim99 <belalpop1999@yahoo.com>
2023-12-13 11:53:15 -0800

    updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,22 +3,19 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * struct listint_s - singly linked list
+ * @n: for integer
+ * @index: index the node in list
+ * @next: pointer to next array
+ *
+ */
 typedef struct listint_s
 {
 	int n;
 	size_t index;
 	struct listint_s *next;
 } listint_t;
-
-typedef struct skiplist_s
-{
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
